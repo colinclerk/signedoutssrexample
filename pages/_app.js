a793383c4a6fad0cdbd8fdf75c6306f84fa41af9
colinclerk <colin@clerk.dev>
2021-07-15 19:49:56 -0700

    create ssr-friend signedout

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,82 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import {
+  ClerkProvider,
+  RedirectToSignIn,
+  SignedIn,
+  SignedOut,
+} from "@clerk/clerk-react";
+import { useRouter } from "next/router";
+
+// Dummy components
+const ApolloProviderWrapper = ({ children }) => <>{children}</>;
+const ApolloProviderUnauthedWrapper = ({ children }) => <>{children}</>;
+const RedirectToAuth = (props) => <RedirectToSignIn {...props} />;
+
+const SignedOutSSR = ({ guaranteedSignedOut, children }) => {
+  if (guaranteedSignedOut) {
+    return <>{children}</>;
+  } else {
+    return <SignedOut>{children}</SignedOut>;
+  }
+};
+
+/**
+ * List pages you want to be publicly accessible, or leave empty if
+ * every page requires authentication. Use this naming strategy:
+ *  "/"              for pages/index.js
+ *  "/foo"           for pages/foo/index.js
+ *  "/foo/bar"       for pages/foo/bar.js
+ *  "/foo/[...bar]"  for pages/foo/[...bar].js
+ */
+const privatePages = [];
+const publicAndPrivatePages = ["/publicPrivate"];
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+
+  // Dummy - remove this
+  const WrappedComponent = Component;
+
+  /**
+   * If the current route is listed as public, render it directly.
+   * Otherwise, use Clerk to require authentication.
+   */
+  return (
+    <ClerkProvider
+      frontendApi="clerk.b1mvy.zxnpf.lcl.dev"
+      navigate={(to) => router.push(to)}
+    >
+      {privatePages.includes(router.pathname) ? (
+        <>
+          <SignedIn>
+            <ApolloProviderWrapper>
+              <WrappedComponent {...pageProps} />
+            </ApolloProviderWrapper>
+          </SignedIn>
+          <SignedOut>
+            <RedirectToAuth returnTo={router.asPath} />
+          </SignedOut>
+        </>
+      ) : publicAndPrivatePages.includes(router.pathname) ? (
+        <>
+          <SignedIn>
+            <ApolloProviderWrapper>
+              <WrappedComponent {...pageProps} />
+            </ApolloProviderWrapper>
+          </SignedIn>
+          <SignedOutSSR guaranteedSignedOut={pageProps.guaranteedSignedOut}>
+            <ApolloProviderUnauthedWrapper>
+              <WrappedComponent {...pageProps} />
+            </ApolloProviderUnauthedWrapper>
+          </SignedOutSSR>
+        </>
+      ) : (
+        <div className="font-sans">
+          <Component {...pageProps} />
+        </div>
+      )}
+    </ClerkProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/publicPrivate.js
@@ -0,0 +1,20 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <meta name="test" content="test" />
+      </Head>
+      This page should SSR
+    </div>
+  );
+}
+
+export async function getServerSideProps(ctx) {
+  return {
+    props: {
+      guaranteedSignedOut: !ctx.req.cookies.__session,
+    },
+  };
+}
